e26a80abbc4420428466ab6e8b5977141187cdfa
Ian-Woo Kim <ianwookim@gmail.com>
2015-12-04 21:49:11 +0000

    clean comments.

--- adb-fhs-shell.nix
+++ adb-fhs-shell.nix
@@ -3,34 +3,52 @@
 with pkgs;
 
 let ndkWrapper = import ./ndk-wrapper.nix { inherit stdenv makeWrapper androidndk; };
-    hsenv = haskell.packages.ghc7102.ghcWithPackages (p: with p; [cabal-install]);
+    hsenv = haskell.packages.ghc7102.ghcWithPackages
+      (p: with p;
+        [ cabal-install
+          hprotoc                  # host protocol buffer code generator
+          protocol-buffers         # host protocol buffer library
+        ]);
     haskell-packages = import ./nixpkgs/top-level/haskell-packages.nix { inherit pkgs callPackage stdenv; };
     ghc-android-env = haskell-packages.packages.ghc-android.ghcWithPackages
-      (p: with p; [ aeson protocol-buffers text-binary network-simple monad-loops ]);
+      (p: with p;
+        [ aeson
+          protocol-buffers         # target protocol buffer library
+          protocol-buffers-descriptor 
+          text-binary
+          network-simple
+          monad-loops ]);
+
+    protobuf-android = import ./protobuf.nix {inherit protobuf androidndk ndkWrapper;};
 
     fhs = buildFHSUserEnv {
             name = "android-env";
             targetPkgs = pkgs: with pkgs;
               [ git gitRepo gnupg python2 curl procps openssl gnumake nettools
                 androidenv.platformTools androidenv.androidsdk_5_1_1_extras
-		androidenv.androidndk
+		androidenv.androidndk 
 		jdk schedtool utillinux m4 gperf
                 perl libxml2 zip unzip bison flex lzop gradle
 		hsenv ghc-android-env ndkWrapper
+                protobuf
+                pkgconfig
               ];
-	    multiPkgs = pkgs: with pkgs; [ zlib ];
+	    multiPkgs = pkgs: with pkgs; [ zlib  ];
             runScript = "bash";
             profile = ''
               export USE_CCACHE=1
               export ANDROID_JAVA_HOME=${jdk.home}
 	      export ANDROID_HOME=${androidenv.androidsdk_5_1_1_extras}/libexec/android-sdk-linux
 	      export ANDROID_NDK_HOME=${androidenv.androidndk}/libexec/android-ndk-r10c
-	      export ANDROID_NDK_ROOT=${androidenv.androidndk}/libexec/android-ndk-r10c	      
+	      export ANDROID_NDK_ROOT=${androidenv.androidndk}/libexec/android-ndk-r10c
+              export PROTOBUF=${protobuf-android}
+              export PKG_CONFIG_PATH=$PROTOBUF/lib/pkgconfig:$PKG_CONFIG_PATH
 	    '';
 	  };
 in stdenv.mkDerivation {
      name = "android-env-shell";
      nativeBuildInputs = [ fhs ];
+     buildInputs = [ protobuf-android  ];
      shellHook = "exec android-env";
 
    }
